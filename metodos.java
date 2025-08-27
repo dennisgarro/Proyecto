13545a4ba286a07ed6019af2ec57e7ce2d1057b2
Diego-Alejo <bstivenacetobon888@gmail.com>
2025-08-26 21:43:38 -0500

    punto 3

--- metodos.java
+++ metodos.java
@@ -1,3 +1,30 @@
+
+import java.util.Scanner;
+
 public class metodos {
-    
+
+    Scanner sc = new Scanner(System.in);
+
+    public ObjLibreria[][] LlenarMatriz(int d) {
+
+        ObjLibreria[][] m = new ObjLibreria[d][d];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                ObjLibreria o = new ObjLibreria();
+                System.out.println("Ingrese el titulo del libro: ");
+                o.setTitulo(sc.next());
+
+                System.out.println("Ingrese el nombre del autor:");
+                o.setAutor(sc.next());
+
+                System.out.println("Ingrese el precio del libro: ");
+                o.setPrecio(sc.nextDouble());
+                m[i][j] = o;
+
+            }
+
+        }
+        return m;
+    }
+
 }
